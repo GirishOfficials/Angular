2c4025687eb96aeff5039af8e01ae534b9a85539
Girish Pawar <107341846+GirishOfficials@users.noreply.github.com>
2022-07-02 18:24:38 +0530

    Form Validation

---
+++ Asgmt 13 & 14/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+// import classes which are required for reactive forms
+import {FormBuilder,FormGroup,Validators, FormControl} from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment14';
+}
